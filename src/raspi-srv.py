81204a43c8470e8eb12a9a1f650f14d2708bc09f
Riz <rize0528@gmail.com>
2017-11-24 03:06:11 +0900

    Append config and function placeholder

--- src/raspi-srv.py
+++ src/raspi-srv.py
@@ -1,9 +1,47 @@
 #!/usr/local/bin/python3
 #
-import sys
+import sys,urllib
+import configparser,argparse
+from bs4 import BeautifulSoup
+#
+def watch_tb():
+  url = "http://rate.bot.com.tw/xrt?Lang=en-US"
+  find_items = [('div .sp-japan-div',2)]
+  #
+  try:
+    html = urllib.urlopen(url).read()
+  except:
+    # Unable to fetch the source from target URL.
+    return "Fail"
+  #
+  try:
+    soup = BeautifulSoup(html)
+  except:
+    # Unable to parse the soup.
+    return "Fail"
+
+  for item in find_items:
+    css_selector, parent_level = item
+    #
 #
 
+#
+def get_serial(port, baud_rate):
+  pass
+
+#
+def get_dweet_payload(dweet_base_url,thing_name):
+  pass
+ 
+#
 def main():
+  #
+  parser = argparse.ArgumentParser(description='''Arduino serial messenger''')
+  parser.add_argument('-c','--conf',default='../conf/conf.ini')
+  args = parser.parse_args()
+  #
+  cfg = configparser.ConfigParser()
+  cfg.read(args.conf)
   pass
 #
 
